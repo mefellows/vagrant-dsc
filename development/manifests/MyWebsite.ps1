cd89dfe34966829a3f21e007e11a0a2b9054451a
Matt Fellows <matt.fellows@onegeek.com.au>
2014-12-06 22:28:05 +1100

    Added a basic module that interacts with IIS; creating a website at http://localhost with a little message

--- development/manifests/MyWebsite.ps1
+++ development/manifests/MyWebsite.ps1
@@ -2,16 +2,29 @@ Configuration MyWebsite
 {
   param ($MachineName)
 
+  Import-DscResource -Module cWebAdministration
+  Import-DscResource -Module MyWebapp
+
   Node $MachineName
   {
-    #Install the IIS Role
-    File website
+    WindowsFeature IIS
     {
-      Ensure = "Present"
-      Type = "Directory"
-      DestinationPath = "c:\mywebroot"
+        Ensure = "Present"
+        Name = "Web-Server"
     }
+    cWebsite DefaultWebsite
+    {
+        Name = "Default Web Site"
+        ApplicationPool = "DefaultAppPool"
+        PhysicalPath = "$env:SystemDrive\inetpub\wwwroot"
+        Ensure = "Absent"
+        DependsOn  = '[WindowsFeature]IIS'
+    }
+        
+    SimpleWebsite sWebsite
+    {
+        WebAppPath = "c:\my-new-webapp"
+        DependsOn  = '[cWebsite]DefaultWebsite'
+    }    
   }
 }
-
-Write-Output "Hello from DSC Provisioner: Config file"
---
+++ development/modules/MyWebapp/DSCResources/SimpleWebsite/SimpleWebsite.schema.psm1
@@ -0,0 +1,44 @@
+﻿Configuration SimpleWebsite
+{
+    param
+    (
+        [String]$WebAppPath = "c:\myWebApp",
+        [String]$WebAppName = "MyWebApp",
+        [HashTable]$AuthenticationInfo = @{Anonymous = "true"; Basic = "false"; Digest = "false"; Windows = "false"}
+    )
+
+    Import-DscResource -Module cWebAdministration
+
+    # Create the new Website    
+    cWebsite Basic
+    {
+        Ensure          = 'Present'
+        Name            = $WebAppName
+        PhysicalPath    = $WebAppPath
+        ApplicationPool = "DefaultAppPool"
+        AuthenticationInfo = SEEK_cWebAuthenticationInformation
+        {
+            Anonymous = $AuthenticationInfo.Anonymous
+            Basic = $AuthenticationInfo.Basic
+            Digest = $AuthenticationInfo.Digest
+            Windows = $AuthenticationInfo.Windows
+        }
+        DependsOn  = '[File]website'
+    }
+
+    File websiteIndex
+    {
+      Ensure = "Present"
+      Type = "File"
+      DestinationPath = "$WebAppPath\index.html"
+      SourcePath = "c:\vagrant\website\index.html"
+      DependsOn  = '[File]website'
+    }
+
+    File website
+    {
+      Ensure = "Present"
+      Type = "Directory"
+      DestinationPath = $WebAppPath
+    }
+}
